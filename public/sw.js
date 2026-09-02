1e5d42b5b6498dc1dc1b0f1f8231a4aeb42b04a4
ASIAN-UBONG SUNDAY UDOFIA <supportdeveloperer@gmail.com>
2026-09-02 20:32:18 +0100

    pwa: allow waiting service worker to activate automatically

--- public/sw.js
+++ public/sw.js
@@ -9,6 +9,10 @@ self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 
+self.addEventListener('message', event => {
+  if (event.data?.type === 'SKIP_WAITING') self.skipWaiting();
+});
+
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(fetch(event.request).then(response => {
